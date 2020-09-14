8173c32a7553ac458e69b12b8dc07ea95f24d323
laskar-ksatria <dock_fest@yahoo.co.id>
2020-09-14 21:36:29 +0700

    adding push to master

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors({ credentials: true, origin: ['http://localhost:3000', 'http://localhost:3001'] }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//main route
+
+app.use(require('./routes'));
+const options = {
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'abels-key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'abels-cert.pem'))
+};
+
+
+
+https.createServer(options, app).listen(PORT, () => {
+    console.log(`HTTPS listening on ${PORT}`);
+});
---
+++ routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const Router = express.Router();
+
+Router.get('/setcookie', (req,res,next) => {
+    let token = 'owlking'
+    res.cookie('servertoken', token);
+    res.status(200).json({message: "Cookie has been set"})
+})
+Router.get('/getcookie', (req,res,next) => {
+    let token = req.cookies.servertoken;
+    res.status(200).json({token})
+})
+
+Router.get('/clearcookie', (req,res,next) => {
+    res.clearCookie('servertoken');
+    res.status(200).json({message: "Cookie has been clear"})
+})
+
+module.exports = Router;
