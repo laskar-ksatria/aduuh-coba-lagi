8a361ad7aef65842be30048888f9d59c19db1e21
laskar-ksatria <dock_fest@yahoo.co.id>
2020-09-14 22:43:29 +0700

    done

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const https = require('https');
-const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
-const PORT = process.env.PORT;
+const PORT = 443;
 const app = express();
 app.use(cors({ credentials: true, origin: ['http://localhost:3000', 'http://localhost:3001'] }));
 app.use(cookieParser());
@@ -15,13 +14,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 //main route
-
 app.use(require('./routes'));
+
+//HTTPS options
 const options = {
     key: fs.readFileSync(path.join(__dirname, 'cert', 'abels-key.pem')),
     cert: fs.readFileSync(path.join(__dirname, 'cert', 'abels-cert.pem'))
 };
-
 
 https.createServer(options, app).listen(PORT, () => {
     console.log(`HTTPS listening on ${PORT}`);
